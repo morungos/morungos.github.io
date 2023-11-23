743787756b4b57535c6afeb3e6a879d902fd7a33
Stuart Watt <stuart@morungos.com>
2023-11-22 22:01:01 -0400

    Updated to Bootstrap 5

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,29 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  let scrollPos = 0;
+  const mainNav = document.getElementById('mainNav');
+  const headerHeight = mainNav.clientHeight;
+  window.addEventListener('scroll', function() {
+      const currentTop = document.body.getBoundingClientRect().top * -1;
+      if ( currentTop < scrollPos) {
+          // Scrolling Up
+          if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+              mainNav.classList.add('is-visible');
+          } else {
+              console.log(123);
+              mainNav.classList.remove('is-visible', 'is-fixed');
+          }
+      } else {
+          // Scrolling Down
+          mainNav.classList.remove(['is-visible']);
+          if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+              mainNav.classList.add('is-fixed');
+          }
+      }
+      scrollPos = currentTop;
+  });
 })
